ab7ce61de5121b1170e51a3efbddff7f50987ce5
Ankur Singh <ankurrana231002@gmail.com>
2023-07-21 11:24:09 +0530

    code

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+//	echo "# github-action" >> README.md
+//	git init
+//	git add README.md
+//	git commit -m "first commit"
+//	git branch -M main
+//	git remote add origin git@github.com:1AnkurSingh/github-action.git
+//	git push -u origin main
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
